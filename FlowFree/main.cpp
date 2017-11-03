2798c43622e80b2bf48fddc77537e85cd893d144
Pirate021 <rezak04@gmail.com>
2017-11-03 21:53:51 +0330

    Creating the matrix and assigning the nodes
    We've created a struct to save the matrix for the flowered game. we tried to make the size of struct at low as possible. we only defined a char variable to identify the color of index. (600 Bytes for the whole matrix in 5x5)
    We will get answer if the game generated as 5x5. (Size didn't determined)
    We are trying to make it better.

--- FlowFree/main.cpp
+++ FlowFree/main.cpp
@@ -6,10 +6,75 @@
 //  Copyright © 2017 Pirate. All rights reserved.
 //
 
-#include <iostream>
+struct node {
+    
+    char color;
+    node * parent;
+    node * next;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+};
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+
+int size = 0;
+int main() {
+    ifstream input("input.txt");
+    
+    short int temp, temp2; //we should delete these temp vars
+    short int colors;
+    
+    node ** array;
+    
+    input >> temp;
+    short int size = temp;
+    array = new node*[temp];
+
+    int size1 = 0;
+    
+    input >> temp;
+    for (int i = 0; i < temp; i++) {
+        array[i] = new node [temp];
+    }
+    for (auto i = 0; i < size; i++)
+        for (auto j =0; j < size; j++) {
+            array[i][j].color = '0';
+    
+        }
+
+    input >> colors;
+    colors = -colors;
+    while (colors != 0) {
+        try {
+            input >> temp; input >> temp2;
+            array[temp-1][temp2-1].color = (char) (colors + 77);
+            cout << temp << " ," << temp2 << " has the color " << array[temp-1][temp2-1].color << endl;
+            
+            input >> temp; input >> temp2;
+            array[temp-1][temp2-1].color = (char) (colors + 77);
+            cout << temp << " ," << temp2 << " has the color " << array[temp-1][temp2-1].color << endl;
+            
+            colors++;
+            
+            cout<<endl;
+        } catch (exception e) {
+            cout<< e.what();
+        }
+        
+    }
+    
+    for (auto i = 0; i < size; i++)
+        for (auto j =0; j < size; j++) {
+            size1 += sizeof(array[i][j]);
+            if (array[i][j].color != '0')
+                cout << array[i][j].color << " ," << " in " << "[" << i << "," << j << "]" <<endl;
+
+        }
+    cout<<endl<<sizeof(array[0][4].parent);
+//    cout<<endl<<sizeof();
+
     return 0;
 }
